f9fe13c1c63a0e1df3f9d12e29eaef5bd7074859
Python Diario <diegocaraballo84@gmail.com>
2016-02-28 17:33:50 -0300

    Update demonio.py
    Se agrega try-except para que el bucle no se corte si ocurre un error.

--- demonio.py
+++ demonio.py
@@ -1,6 +1,7 @@
 from twython import Twython
 import random
 import os
+import time
 
 # Claves de la aplicacion Twitter
 
@@ -13,17 +14,23 @@ twitter = Twython(app_key, app_secret, oauth_token, oauth_token_secret)
 def leertxtenlista():
 
  # Bucle infinito
- mensaje = ""
+ try:
+  mensaje = ""
 
- # Abrimos el archivo
- archi=open('twit','r')
- lineas=archi.readlines()
+  # Abrimos el archivo
+  archi=open('twit','r')
+  lineas=archi.readlines()
 
- # Elegir un twit aleatorio
- t = random.randint(0,len(lineas)-1)
- mensaje = lineas[t]
- # Agrega el Tweet
- twitter.update_status(status=mensaje)
- archi.close()
-
+  # Elegir un twit aleatorio
+  t = random.randint(0,len(lineas)-1)
+  mensaje = lineas[t]
+  # Agrega el Tweet
+  twitter.update_status(status=mensaje)
+  archi.close()
+  
+ except:
+  
+  time.sleep(3)
+  pass
+ 
 leertxtenlista()
